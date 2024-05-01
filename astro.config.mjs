154c4796ca299a38d4fa6a2ddea115428c4e76bb
Frederick <frederick.blaesing@proton.me>
2024-05-01 18:00:48 +0000

    configure for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
   redirects: {
-    "/": "/posts",
+    "/": "/posts"
   },
+  output: "server",
+  adapter: vercel()
 });
