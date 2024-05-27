c26d5fb7949fb92930fc1b7e5a0028e9e4f7cb81
frederick <git@blaesing.me>
2024-05-27 21:38:58 +0200

    add links to footer

--- db/seed.ts
+++ db/seed.ts
@@ -1,5 +1,5 @@
 import { db, Forum, User, Member, Post, Vote } from "astro:db";
-import UniqueIds from "@src/lib/UniqueIds";
+import UniqueIds from "@lib/UniqueIds";
 
 export default async function () {
   const forums = UniqueIds();
