eff610fb3b453ac77fa4b76f4eefcf8f28aabc7b
frederick <git@blaesing.me>
2024-06-06 17:34:11 +0200

    add support for invidious videos

---
+++ src/lib/youTube.ts
@@ -0,0 +1,24 @@
+export async function getYouTubeEmbedUrl(href: URL | string) {
+  let url = new URL(href);
+  const hostname = url.hostname.replace("www.", "");
+
+  if (hostname === "youtu.be")
+    url = new URL(`https://www.youtube.com/watch?v=${url.pathname}`);
+
+  const instances = ["youtube.com", ...(await getInvidiousInstances())];
+  if (!instances.includes(hostname)) return;
+
+  const videoId = url.searchParams.get("v");
+  return `https://${url.hostname}/embed/${videoId}`;
+}
+
+let invidiousInstances: string[] | null = null;
+
+export async function getInvidiousInstances() {
+  if (!invidiousInstances) {
+    const response = await fetch("https://api.invidious.io/instances.json");
+    const data: [string, any][] = await response.json();
+    invidiousInstances = data.map((v) => v[0]);
+  }
+  return invidiousInstances;
+}
