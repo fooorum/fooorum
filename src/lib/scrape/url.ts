22b92ec502a1eeb088177f174f0b8f9bdb635805
frederick <git@blaesing.me>
2024-06-12 13:23:26 +0200

    fallback to filename as image alt

--- src/lib/scrape/url.ts
+++ src/lib/scrape/url.ts
@@ -2,3 +2,9 @@ export function getFallbackTitle(url: string | URL) {
   const { hostname } = new URL(url);
   return hostname;
 }
+
+export function getFallbackAlt(url: string | URL) {
+  const { hostname, pathname } = new URL(url);
+  const path = hostname + pathname;
+  return path.replace(/\/+$/, "").split("/").pop()!;
+}
